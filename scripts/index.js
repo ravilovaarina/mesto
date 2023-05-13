929e3e58528ebfabaa1c2462182907b67bad1348
Arina Ravilova <ravilova.arina06@gmail.com>
2023-05-13 14:47:20 +0300

    feat:delete cards

--- scripts/index.js
+++ scripts/index.js
@@ -17,32 +17,33 @@ const closePopupAddButton = document.querySelector('.popup__button-closed_type_a
 // объявление переменных для карточек
 const cardTemplate = document.querySelector('#cards__item').content;
 const cards = document.querySelector('.cards');
-const initialCards = [
+let initialCards = [
     {
-      name: '2009',
-      link: 'https://images.unsplash.com/photo-1561871733-40a3338b8cb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=770&q=80'
+        name: '2009',
+        link: 'https://images.unsplash.com/photo-1561871733-40a3338b8cb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=770&q=80'
     },
     {
-      name: '2008',
-      link: 'https://images.unsplash.com/photo-1598461336473-b0cc035d4e35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1768&q=80'
+        name: '2008',
+        link: 'https://images.unsplash.com/photo-1598461336473-b0cc035d4e35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1768&q=80'
     },
     {
-      name: '2007',
-      link: 'https://images.unsplash.com/photo-1601711187757-06a086b27e54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=792&q=80'
+        name: '2007',
+        link: 'https://images.unsplash.com/photo-1601711187757-06a086b27e54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=792&q=80'
     },
     {
-      name: '2006',
-      link: 'https://images.unsplash.com/photo-1585022564445-3523e9b75f26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        name: '2006',
+        link: 'https://images.unsplash.com/photo-1585022564445-3523e9b75f26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
     },
     {
-      name: '2005',
-      link: 'https://images.unsplash.com/photo-1575795325632-377ca781cf78?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2932&q=80'
+        name: '2005',
+        link: 'https://images.unsplash.com/photo-1575795325632-377ca781cf78?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2932&q=80'
     },
     {
-      name: '2004',
-      link: 'https://images.unsplash.com/photo-1556515268-97d056bdb5a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1768&q=80'
+        name: '2004',
+        link: 'https://images.unsplash.com/photo-1556515268-97d056bdb5a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1768&q=80'
     }
-  ];
+];
+
 
 // функции для popup
 function popupOpen(popupElement) {
@@ -68,29 +69,38 @@ function handleFormSubmit(evt) {
 //функции для popup_type_add
 function addFormSubmit(evt) {
     evt.preventDefault();
-    const cardsElement = cardTemplate.querySelector('.cards__item').cloneNode(true);
+    let newCard = {
+        name: placeInput.value,
+        link: urlInput.value
+    };
+    initialCards.unshift(newCard);
 
-    cardsElement.querySelector('.cards__pic').src =  urlInput.value;
-    cardsElement.querySelector('.cards__text').textContent = placeInput.value;
+    const cardsElement = cardTemplate.querySelector('.cards__item').cloneNode(true);
+    cardsElement.querySelector('.cards__pic').src = initialCards[0].link;
+    cardsElement.querySelector('.cards__text').textContent = initialCards[0].name;
     cards.prepend(cardsElement);
-    popupClose(popupAdd);
+
+    initialCards.forEach(function () {
+        const trash = cardsElement.querySelector('.cards__button-delete');
+        trash.addEventListener('click', deleteCard);
+    })
 }
 // вызовы функция для popup_type_add
-openPopupAddButton.addEventListener('click', function(){
+openPopupAddButton.addEventListener('click', function () {
     popupOpen(popupAdd);
 });
 
-closePopupAddButton.addEventListener('click', function() {
+closePopupAddButton.addEventListener('click', function () {
     popupClose(popupAdd);
 })
 
 // вызовы функция для popup_type_edit
-openPopupEditButton.addEventListener('click', function(){
+openPopupEditButton.addEventListener('click', function () {
     popupOpen(popupEdit);
 });
 openPopupEditButton.addEventListener('click', openPopupEdit);
 
-closePopupEditButton.addEventListener('click', function() {
+closePopupEditButton.addEventListener('click', function () {
     popupClose(popupEdit);
 })
 
@@ -100,15 +110,18 @@ formPopupEdit.addEventListener('submit', handleFormSubmit);
 formPopupAdd.addEventListener('submit', addFormSubmit);
 
 // карточки
-for (let i = 0; i < initialCards.length; i += 1){
+for (let i = 0; i < initialCards.length; i += 1) {
     const cardsElement = cardTemplate.querySelector('.cards__item').cloneNode(true);
     cardsElement.querySelector('.cards__pic').src = initialCards[i].link;
     cardsElement.querySelector('.cards__text').textContent = initialCards[i].name;
-    cards.append(cardsElement); 
+    cards.append(cardsElement);
+    const trash = cardsElement.querySelector('.cards__button-delete');
+    trash.addEventListener('click', deleteCard);
 }
 
-// удаление карточки мяу)
-const deletecard = (evt) => {
-    const card = evt.closest('.cards__item');
+// удаление карточкек
+function deleteCard(evt) {
+    const card = evt.target.closest('.cards__item');
     card.remove();
+    console.log(card)
 }
